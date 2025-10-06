82110d62dab239b0bc6e4d3ab7a1c3b5401a3812
Urav Tanna <82610686+MorbidMight@users.noreply.github.com>
2025-10-06 13:43:35 -0400

    made App deploy friendly

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
-import Admin from "./Components/Admin/admin";
+import {Admin} from "./Components/Admin/admin";
 import ProtectedRoute from "./ProtectedRoute";
 import Dashboard from "./Components/Admin/Dashboard";
 
